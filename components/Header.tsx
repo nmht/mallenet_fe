4b7c4fd82a15f6041a815e943a3cdb2a7247f636
nmhoangthien <91450889+nmht@users.noreply.github.com>
2024-11-22 17:23:07 +0700

    done sidebar for sprint 1

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-white py-3 px-5 border-b border-b-slate-200">
+    <header className="flex items-center justify-between bg-white py-3 px-5 border-b border-b-slate-200 h-20">
       <div className="text-2xl font-bold text-black">Mallenet</div>
       <input type="text" className="search-bar" placeholder="Search" />
       <div className="actions">
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,27 +1,11 @@
-export default function Sidebar() {
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/AppSidebar"
+
+export default function Sidebar({ children }: { children: React.ReactNode }) {
   return (
-    <aside className="w-20 lg:w-72 bg-white border-r border-r-slate-200 px-5 py-3">
-      <nav id="nav-feature">
-        <ul>
-          <li className="active">For You</li>
-          <li>Explore</li>
-          <li>Following</li>
-          <li>Friends</li>
-          <li>LIVE</li>
-          <li>Messages</li>
-          <li>Profile</li>
-        </ul>
-      </nav>
-      <nav id="nav-feature">
-        <h4>Following accounts</h4>
-        <ul>
-          <li>_ttngz.1310_</li>
-          <li>vuxminhtus</li>
-          <li>deptraiihiyeuhi</li>
-          <li>_myduyen55_</li>
-          <li>julie0237_</li>
-        </ul>
-      </nav>
-    </aside>
+    <SidebarProvider>
+      <AppSidebar />
+      {children}
+    </SidebarProvider>
   );
 }
